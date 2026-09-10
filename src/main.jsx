8b00055a6018529b2c77ae786caa7b0360f61074
johnymathias17-afk <johnymathias17@gmail.com>
2026-09-10 10:40:28 -0300

    Activate editorial duplicate suppression

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './editorial.css'
 import './pro-teaser.css'
 import './pro-teaser.js'
 import './seo-enhancer.js'
+import './editorial-enhancer.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
