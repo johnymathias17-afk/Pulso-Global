c867bcfb5566fdef24ac4957fba8ee12f8749661
johnymathias17-afk <johnymathias17@gmail.com>
2026-09-13 00:12:30 -0300

    Add editorial trust pages to sitemap

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -2,12 +2,16 @@ const SUPABASE_URL = process.env.VITE_SUPABASE_URL
 const SUPABASE_KEY = process.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
 function esc(value = '') {
-  return String(value).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;')
+  return String(value).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\"/g, '&quot;').replace(/'/g, '&apos;')
 }
 
 export default async function handler(req, res) {
   try {
-    const urls = ['https://vetorglobal.com.br/']
+    const urls = [
+      '<url><loc>https://vetorglobal.com.br/</loc><changefreq>hourly</changefreq><priority>1.0</priority></url>',
+      '<url><loc>https://vetorglobal.com.br/metodologia</loc><changefreq>monthly</changefreq><priority>0.7</priority></url>',
+      '<url><loc>https://vetorglobal.com.br/redacao</loc><changefreq>monthly</changefreq><priority>0.7</priority></url>'
+    ]
     if (SUPABASE_URL && SUPABASE_KEY) {
       const endpoint = `${SUPABASE_URL}/rest/v1/articles?select=id,published_at,created_at&order=published_at.desc&limit=500`
       const response = await fetch(endpoint, { headers: { apikey: SUPABASE_KEY, Authorization: `Bearer ${SUPABASE_KEY}` } })
@@ -21,7 +25,7 @@ export default async function handler(req, res) {
         }
       }
     }
-    const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.map(url => url.startsWith('<url>') ? url : `<url><loc>${url}</loc><changefreq>hourly</changefreq><priority>1.0</priority></url>`).join('')}</urlset>`
+    const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.join('')}</urlset>`
     res.setHeader('Content-Type', 'application/xml; charset=utf-8')
     res.setHeader('Cache-Control', 'public, s-maxage=900, stale-while-revalidate=3600')
     return res.status(200).send(xml)
